26b653ae9520d72cf3d6e1a403f8b936e563bae8
Michael Jordan <michael@glowingpixel.com>
2022-01-04 20:45:52 -0800

    Simple test

--- tests/test_pkl.py
+++ tests/test_pkl.py
@@ -1,7 +1,12 @@
 from imflib.pkl import Pkl
 import sys
 
+if len(sys.argv) < 2:
+	sys.exit("Usage: test_pkl.py path_to_pkl")
+
 pkl = Pkl.fromFile(sys.argv[1])
 
+print(pkl.id)
+
 for asset in pkl.assets:
-	print(f"{asset.name.ljust(100)}  {asset.urn}")
+	print(f"{asset.file_name.ljust(100)}  {asset.id}")
