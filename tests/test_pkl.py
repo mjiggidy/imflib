ee113d8b2a8ee0d858626336399bd7db6e10ddec
Michael Jordan <michael@glowingpixel.com>
2022-02-16 19:43:19 -0800

    Addtl testing

--- tests/test_pkl.py
+++ tests/test_pkl.py
@@ -1,12 +1,18 @@
 from imflib.pkl import Pkl
-import sys
+import sys, pathlib
 
 if len(sys.argv) < 2:
-	sys.exit("Usage: test_pkl.py path_to_pkl")
+	sys.exit("Usage: test_pkl.py path_to_imf [path_to_imf ...]")
 
-pkl = Pkl.fromFile(sys.argv[1])
 
-print(pkl.id)
+for path_imf in sys.argv[1:]:
 
-for asset in pkl.assets:
-	print(f"{asset.file_name.ljust(100)}  {asset.id}")
+	path_pkls = list(pathlib.Path(path_imf).glob("PKL_*"))
+	print(f"{path_imf} has {len(path_pkls)} PKL(s):")
+	for path_pkl in path_pkls:
+		pkl = Pkl.fromFile(path_pkl)
+		print(f"{path_pkl.name}:")
+		print(f"Created on {pkl.issue_date} by {pkl.issuer} using {pkl.creator}")
+		print(f"Annotations: {pkl.annotation_text}")
+		print(f"Contains {len(pkl.assets)} asset(s) totalling {pkl.total_size/1000/1000/1000:.2f} GB")
+		print("---")
