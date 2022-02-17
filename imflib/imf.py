ebacb9c7c1bbd524228781f3c8cfeb8a47217f30
Michael Jordan <michael@glowingpixel.com>
2022-02-16 19:44:38 -0800

    Integrate assetmap

--- imflib/imf.py
+++ imflib/imf.py
@@ -1,44 +1,12 @@
-from imflib import cpl, pkl
-import datetime
-import pathlib, typing, dataclasses, re
-
-def xsd_datetime_to_datetime(xsd_datetime:str)->datetime:
-	"""Convert XML XSD DateTime to python datetime"""
-	# XSD DateTime format is:
-	# YYYY-MM-DDTHH:MM:SS(.SS)
-	#
-	# Followed optionally by
-	# Z        - UTC
-	# +|-HH:MM - Timezone offset from UTC
-
-	match_date = re.match(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2}(?:\.\d+)?)(?P<timezone>(?:Z)|([\+\-].+))?", xsd_datetime, re.I)
-	if not match_date:
-		raise Exception(f"Invalid XSD DateTime: {xsd_datetime}")
-
-	# Reconcile timezone situation
-	if match_date.group("timezone") is None or match_date.group("timezone").lower() == 'z':
-		tz_delta = datetime.timezone.utc
-	else:
-		tz_h, tz_m = tuple([float(x) for x in match_date.group("timezone").split(':')])
-		tz_delta = datetime.timezone(datetime.timedelta(hours=tz_h, minutes=tz_m))
-	
-	# Here we go
-	return datetime.datetime(
-		year        = int(match_date.group("year")),
-		month       = int(match_date.group("month")),
-		day         = int(match_date.group("day")),
-		hour        = int(match_date.group("hour")),
-		minute      = int(match_date.group("minute")),
-		second      = int(match_date.group("second").split('.')[0]),
-		microsecond = int(match_date.group("second").split('.')[1] if '.' in match_date.group("second") else 0),
-		tzinfo      = tz_delta
-	)
+from imflib import cpl, pkl, assetmap
+import pathlib, typing, dataclasses
 
 
 @dataclasses.dataclass
 class Imf:
 	"""An IMF package"""
 
+	asset_map:assetmap.AssetMap
 	cpl:cpl.Cpl
 	pkl:pkl.Pkl
 
@@ -51,20 +19,23 @@ class Imf:
 		if not path_imf.is_dir():
 			raise NotADirectoryError(f"Path does not exist or is not a directory: {path_imf}")
 		
+		input_assetmap = assetmap.AssetMap.fromFile(pathlib.Path(path_imf,"ASSETMAP.xml"))
+		if len(input_assetmap.packing_lists) != 1:
+			raise NotImplementedError(f"Support for {len(input_assetmap.packing_lists)} PKLs is not yet implemented")
+		elif len(input_assetmap.packing_lists[0].chunks) != 1:
+			raise NotImplementedError("Support for chunked PKLs is not yet implemented")
+		
+		input_pkl = pkl.Pkl.fromFile(pathlib.Path(path_imf,input_assetmap.packing_lists[0].file_paths[0]))
+		
 		glob_temp = list(path_imf.glob("CPL*.xml"))
 		if len(glob_temp) != 1:
 			raise FileNotFoundError("Could not find a CPL in this directory")
 
 		input_cpl = cpl.Cpl.fromFile(str(glob_temp[0]))
 
-		glob_temp = list(path_imf.glob("PKL*.xml"))
-		if len(glob_temp) != 1:
-			raise FileNotFoundError("Could not find a PKL in this directory")
-
-		input_pkl = pkl.Pkl.fromFile(str(glob_temp[0]))
 
 		# Marry the pkl assets to the CPL
 		for res in input_cpl.resources:
 			res.setAsset(input_pkl.getAsset(res.file_id))
 		
-		return cls(input_cpl, input_pkl)
+		return cls(input_assetmap, input_cpl, input_pkl)
