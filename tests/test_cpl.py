dab42918c4d07d15895d1a141070309f07198774
Michael Jordan <michael@glowingpixel.com>
2022-02-23 21:04:53 -0800

    Functional testing

--- tests/test_cpl.py
+++ tests/test_cpl.py
@@ -7,4 +7,14 @@ if len(sys.argv) < 2:
 
 for path_imf in sys.argv[1:]:
 	for path_cpl in pathlib.Path(path_imf).glob("CPL_*.xml"):
-		print(cpl.Cpl.fromFile(path_cpl))
+		imf_cpl = cpl.Cpl.fromFile(path_cpl)
+		print(f"{path_cpl}")
+		print(f"CPL Name: {imf_cpl.title}")
+		if imf_cpl.annotation: print(imf_cpl.annotation)
+		print(f"Start Timecode: {imf_cpl.tc_start}")
+
+		reslist = list(imf_cpl.resources)
+		print(f"Contains {len(list(reslist))} resources in {len(list(imf_cpl.sequences))} sequences across {len(list(imf_cpl.segments))} segment(s):")
+		print(f"  {len([res for res in reslist if isinstance(res,cpl.ImageResource)])} videos, {len([res for res in reslist if isinstance(res, cpl.AudioResource)])} audio")
+
+		print("---")
