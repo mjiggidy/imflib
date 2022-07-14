2e444f52e8add1cc3a6811176255f043672a5255
mjiggidy <michael@glowingpixel.com>
2022-07-14 15:30:29 -0700

    Clarifications and obsessions

--- imflib/pkl.py
+++ imflib/pkl.py
@@ -1,11 +1,11 @@
-# Based on SMPTE 429-8-2007
-# https://ieeexplore.ieee.org/document/7290849?arnumber=7290849
+# Based on SMPTE 429-8-2007: https://ieeexplore.ieee.org/document/7290849
+# With additions from SMPTE 2067-2-2020: https://ieeexplore.ieee.org/document/9097478
 
 import dataclasses, typing, datetime
 import xml.etree.ElementTree as et
 from imflib import xsd_datetime_to_datetime, xsd_optional_string
 
-@dataclasses.dataclass()
+@dataclasses.dataclass(frozen=True)
 class Asset:
 	"""An asset packed into this IMF package"""
 	
@@ -15,7 +15,9 @@ class Asset:
 	hash:str
 	"""Base64-encoded message digest of the asset"""
 
-	hash_type:str
+	hash_algorithm:str # Addition via SMPTE 2067-2
+	"""Name of the digest type used by the hash"""
+
 	size:int
 	"""File size of the asset in bytes"""
 
@@ -28,24 +30,25 @@ class Asset:
 	annotation_text:typing.Optional[str]=""
 	"""Optional description of the asset"""
 
-
-
 	@classmethod
 	def from_xml(cls, xml:et.Element, ns:dict) -> list["Asset"]:
 		"""Create an asset from an XML element"""
 
 		id = xml.find("Id", ns).text
-		hash = xml.find("Hash", ns).text
-		hash_type = xml.find("HashAlgorithm", ns).attrib.get("Algorithm").split("#")[-1] # TODO: Only SHA-1 is currently supported. Maybe hard-code this?
 		size = int(xml.find("Size", ns).text)
 		type = xml.find("Type", ns).text
+		
+		# As of 2067-2-2020, http://www.w3.org/2000/09/xmldsig#sha1 is the only supported algorithm
+		hash = xml.find("Hash", ns).text
+		hash_algorithm = xml.find("HashAlgorithm", ns).attrib.get("Algorithm").split("#")[-1]
+		
 		original_file_name = xsd_optional_string(xml.find("OriginalFileName", ns))
 		annotation_text = xsd_optional_string(xml.find("AnnotationText", ns))
 		
 		return cls(
 			id=id,
 			hash=hash,
-			hash_type=hash_type,
+			hash_algorithm=hash_algorithm,
 			size=size,
 			type=type,
 			original_file_name=original_file_name,
