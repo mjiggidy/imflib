75d8679a167fcde47729c3ff7dc997b483644be8
mjiggidy <michael@glowingpixel.com>
2022-07-14 17:58:39 -0700

    VOLINDEX deprecated

--- imflib/assetmap.py
+++ imflib/assetmap.py
@@ -1,7 +1,8 @@
 # Based on SMPTE 429-8-2007: https://ieeexplore.ieee.org/document/7290438
 
 # NOTE:
-# ASSETMAP.xml provides the mapping between PKL UUIDs and file locations on a volume
+# ASSETMAP.xml is the entry point into the package
+# AssetMap provides the mapping between PKL UUIDs and file locations on a volume
 # The assetmap must exist on each volume it references
 # If multiple volumes are referenced, a VOLINDEX.xml file must be present with a unique index on each volume
 # The assetmap may contain mappings for more than one package
@@ -23,7 +24,7 @@ class AssetMap:
 	creator:str
 	"""The facility or system that created this asset map"""
 
-	volume_count:int	# TODO: Make dynamic property based on asset chunks?
+	volume_count:int	# TODO: Per SMPTE 0429-9-2014 update, "Volume Count shall be 1."  So uh...
 	"""Total number of volumes referenced by this asset map"""
 
 	issue_date:datetime.datetime
@@ -81,7 +82,7 @@ class AssetMap:
 			if asset.id == id: return asset		
 		return None
 
-
+# TODO: Per SMPTE 0429-9-2014 update, "The VolumeIndex structure is not used."  Look into the existence of any 2007-era spec'd IMFs?
 @dataclasses.dataclass(frozen=True)
 class VolumeIndex:
 	"""A `VolumeIndex` file required only in multi-volume packages"""
@@ -156,7 +157,7 @@ class Chunk:
 	file_path:str
 	"""Relative file path to the chunk"""
 
-	volume_index:int=1
+	volume_index:int=1 # TODO: Per SMPTE 0429-9-2014 update, "Volume Count shall be 1."
 	"""Index of the volume containing the chunk"""
 
 	offset:int=0
