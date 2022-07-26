b379d11cb8c272e683293545588ae07dc61a8089
mjiggidy <michael@glowingpixel.com>
2022-07-25 20:05:06 -0700

    CPL improvements

--- imflib/cpl.py
+++ imflib/cpl.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as et
-import dataclasses, typing, re, abc, datetime
+import dataclasses, typing, re, abc, datetime, uuid
 from posttools import timecode
-from imflib import xsd_datetime_to_datetime, xsd_optional_string, xsd_optional_integer
+from imflib import xsd_datetime_to_datetime, xsd_optional_string, xsd_optional_integer, xsd_optional_bool, xsd_optional_usertext, xsd_optional_security, UserText, Security
 
 pat_nsextract = re.compile(r'^\{(?P<uri>.+)\}(?P<name>[a-z0-9]+)',re.I)
 
@@ -370,34 +370,50 @@ class ContentMaturityRating:
 
 @dataclasses.dataclass(frozen=True)
 class Locale:
-	"""Localization info"""
+	"""Locale-specific information"""
 
-	annotation:typing.Optional[str]=None
-	languages:typing.Optional[list[str]]=None
-	regions:typing.Optional[list[str]]=None
-	content_maturity_ratings:typing.Optional[list[ContentMaturityRating]]=None
+	languages:list[str]=dataclasses.field(default_factory=list)
+	"""RFC 5646 language tags of the audiences in this locale"""
+
+	regions:list[str]=dataclasses.field(default_factory=list)
+	"""RFC 5646 region subtags in this locale"""
+
+	content_maturity_ratings:list[ContentMaturityRating]=dataclasses.field(default_factory=list)
+	"""Content maturity ratings for this locale"""
+
+	annotation:typing.Optional[UserText]=None
+	"""Description of this locale"""
 
 	@classmethod
-	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None)->"Locale":
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None) -> "Locale":
 		"""Parse a LocaleType from a LocaleListType"""
-		annotation = xsd_optional_string(xml.find("Annotation",ns))
+
+		annotation = xsd_optional_usertext(xml.find("Annotation",ns))
 		
-		languages = []
-		for lang_list in xml.findall("LanguageList",ns):
-			for lang in lang_list.findall("Language",ns):
+		languages = list()
+		ll = xml.find("LanguageList",ns)
+		if ll:
+			for lang in ll:
 				languages.append(lang.text)
 		
-		regions = []
-		for reg_list in xml.findall("RegionList",ns):
-			for reg in reg_list.findall("Region",ns):
+		regions = list()
+		rl = xml.find("RegionList",ns)
+		if rl:
+			for reg in rl:
 				regions.append(reg.text)
 
 		ratings = []
-		for r_list in xml.findall("ContentMaturityRatingList",ns):
-			for rating in r_list.findall("ContentMaturityRating",ns):
+		ral = xml.find("ContentMaturityRatingList",ns)
+		if ral:
+			for rating in ral:
 				ratings.append(ContentMaturityRating.from_xml(rating, ns))
 			
-		return cls(annotation, languages, regions, ratings)
+		return cls(
+			annotation=annotation,
+			languages=languages,
+			regions=regions,
+			content_maturity_ratings=ratings
+		)
 		
 
 
@@ -478,42 +494,109 @@ class EditRate:
 	
 	def __str__(self) -> str:
 		return str(round(self.decimal)) if self.decimal.is_integer() else str(round(self.decimal,2))
-		
+
+@dataclasses.dataclass(frozen=True)
+class ContentKind:
+	"""The kind of content undelying the composition"""
+
+	kind:str
+	"""The kind of content"""
+	# TODO: Validate default against allowed types?
+
+	scope:typing.Optional[str]="http://www.smpte-ra.org/schemas/2067-3/2013#content-kind"
+	"""The defined set of possible kinds"""
+
+	def __str__(self) -> str:
+		return self.kind
+	
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None) -> "ContentKind":
+		"""Parse ContentKind from XML"""
+
+		return cls(
+			kind=xml.text,
+			scope=xml.attrib.get("scope")
+		)
 		
 @dataclasses.dataclass(frozen=True)
 class Cpl:
 	"""An IMF Composition Playlist"""
 
-	id:str
+	id:uuid.UUID
+	"""Unique identifier for this CPL encoded as a urn:UUID [RFC 4122]"""
+
 	issue_date:datetime.datetime
-	title:str
+	"""Datetime this asset map was issued"""
+
+	title:UserText
+	"""The title of the content in this CPL"""
+
 	edit_rate:EditRate
+	"""The granularity of time-related parameters used in this CPL"""
+
 	_segments:list["Segment"]
 	
-	annotation:str=""
-	issuer:str=""
-	creator:str=""
-	content_originator:str=""
-	content_kind:str=""
-	runtime:str=""
+	annotation:typing.Optional[UserText]=None
+	"""Optional description of this CPL"""
 
-	tc_start:timecode.Timecode=None
-	content_versions:list["ContentVersion"]=None
-	locales:list["Locale"]=None
+	issuer:typing.Optional[UserText]=None
+	"""The person or company that issued this CPL"""
+
+	creator:typing.Optional[UserText]=None
+	"""The facility or system that created this CPL"""
+
+	content_originator:typing.Optional[UserText]=None
+	"""The person or company that produced the content used in this CPL"""
+
+	content_kind:typing.Optional[ContentKind]=None
+	"""The kind of work represented by this CPL"""
+
+	total_runtime:typing.Optional[datetime.timedelta]=None
+	"""The approximate runtime of the content in this CPL, for informational purposes only"""
+
+	tc_start:typing.Optional[timecode.Timecode]=None
+	"""The starting timecode of this CPL"""
+
+	content_versions:set["ContentVersion"]=dataclasses.field(default_factory=set)
+	"""The specific version or revision of the content in this CPL"""
+	#TODO: "No two ContentVersion elements shall have identical Id elements."
+	#TODO: "Two Composition Playlist instances shall be assumed to refer to the same 
+	# content if they have in common at least one Id element of a ContentVersion element."
+	#TODO: `dict` instead?
+
+	locales:list["Locale"]=dataclasses.field(default_factory=list)
+	"""The intended audiences for this CPL"""
 
 	# Still need
-	essence_descriptors:list["EssenceDescriptor"]=None
-	extension_properties:list["ExtensionProperty"]=None
-	signer:Signer=None
-	signature:Signature=None
+	essence_descriptors:list["EssenceDescriptor"]=dataclasses.field(default_factory=list)
+	"""Descriptors intended for scheduling and tracking content referenced by this CPL"""
+
+	extension_properties:list["ExtensionProperty"]=dataclasses.field(default_factory=list)
+	"""Additional metadata describing this CPL"""
+
+	security:typing.Optional[Security]=None
+	"""XML signer and signature"""
 
 	@staticmethod
-	def timecode_from_composition(xml:et.Element, ns:typing.Optional[dict]=None)->timecode.Timecode:
+	def xsd_optional_compositiontimecode(xml:et.Element, ns:typing.Optional[dict]=None, default:typing.Optional[timecode.Timecode]=None) -> timecode.Timecode:
 		"""Return a Timecode object from an XSD CompositionTimecodeType"""
+
+		if xml is None:
+			return default
+
 		tc_addr = xml.find("TimecodeStartAddress",ns).text
 		tc_rate = int(xml.find("TimecodeRate",ns).text)
-		tc_drop = xml.find("TimecodeDropFrame",ns).text == 1
+		tc_drop = xsd_optional_bool(xml.find("TimecodeDropFrame",ns))
 		return timecode.Timecode(tc_addr, tc_rate, timecode.Timecode.Mode.DF if tc_drop else timecode.Timecode.Mode.NDF)
+	
+	@staticmethod
+	def xsd_optional_runtime(xml:et.Element, ns:typing.Optional[dict]=None, default:typing.Optional[datetime.timedelta]=None) -> datetime.timedelta:
+		"""Return a `datetime.timedelta` object based on a given runtime"""
+		if xml is None:
+			return default
+		
+		runtime = [int(x) for x in xml.text.split(":")]
+		return datetime.timedelta(hours=int(runtime[0], minutes=runtime[1], seconds=runtime[2]))
 	
 	@classmethod
 	def from_file(cls, path:str) -> "Cpl":
@@ -528,64 +611,53 @@ class Cpl:
 		Intended to be called from Cpl.from_file(), but you do you.
 		"""
 		
-		id = xml.find("Id",ns).text
+		id = uuid.UUID(xml.find("Id",ns).text)
 		issue_date = xsd_datetime_to_datetime(xml.find("IssueDate",ns).text)
 		title = xml.find("ContentTitle",ns).text
 
-		annotation = xsd_optional_string(xml.find("Annotation",ns))
-		issuer = xsd_optional_string(xml.find("Issuer",ns))
-		creator = xsd_optional_string(xml.find("Creator",ns))
-		content_originator = xsd_optional_string(xml.find("ContentOriginator",ns))
-		content_kind = xsd_optional_string(xml.find("ContentKind",ns))
+		annotation = xsd_optional_usertext(xml.find("Annotation",ns))
+		issuer = xsd_optional_usertext(xml.find("Issuer",ns))
+		creator = xsd_optional_usertext(xml.find("Creator",ns))
+		content_originator = xsd_optional_usertext(xml.find("ContentOriginator",ns))
+		content_kind = xsd_optional_usertext(xml.find("ContentKind",ns))
 
 		# Rate and timecode
 		edit_rate = EditRate.from_xml(xml.find("EditRate",ns), ns)
-		tc_start = cls.timecode_from_composition(xml.find("CompositionTimecode",ns),ns) if xml.find("CompositionTimecode",ns) is not None \
-			else timecode.Timecode("00:00:00:00",float(edit_rate))
+		tc_start = cls.xsd_optional_compositiontimecode(xml.find("CompositionTimecode",ns),ns)
 
 		# Runtime is just hh:mm:ss and not to be trusted
-		runtime = xsd_optional_string(xml.find("TotalRuntime",ns))
+		runtime = cls.xsd_optional_runtime(xml.find("TotalRuntime",ns))
 		
 		# ContentVersionList
 		content_versions = list()
-		if xml.find("ContentVersionList",ns) is not None:
-			for cv in xml.find("ContentVersionList",ns).findall("ContentVersion",ns):
+		cvl = xml.find("ContentVersionList",ns)
+		if cvl:
+			for cv in cvl:
 				content_versions.append(ContentVersion.from_xml(cv,ns))
 		
 		# Locales
 		locale_list = list()
-		for loc_list in xml.findall("LocaleList",ns):
-			for locale in loc_list.findall("Locale",ns):
+		ll = xml.find("LocaleList",ns)
+		if ll:
+			for locale in ll:
 				locale_list.append(Locale.from_xml(locale,ns))
 		
-		# Application extensions
-		extensions_list = []
-		for prop_list in xml.findall("ExtensionProperties",ns):
-			for prop in prop_list:
+		# Extensions
+		extensions_list = list()
+		el = xml.find("ExtensionProperties",ns)
+		if el:
+			for prop in el:
 				extensions_list.append(ExtensionProperty.from_xml(prop,ns))
-		
-		# Signer
-		# TODO: Fully implement
-		if xml.find("Signer",ns) is not None:
-			signer = Signer.from_xml(xml.find("Signer",ns),ns)
-		else:
-			signer=None
-		
-		# Signature
-		# TODO: Fully implement
-		if xml.find("ds:Signature",{"ds":"http://www.w3.org/2000/09/xmldsig#"}) is not None:
-			signature = Signature.from_xml(xml.find("ds:Signature",{"ds":"http://www.w3.org/2000/09/xmldsig#"}), ns)
-		else:
-			signature=None
-		
-		# TODO: Signer and signature must be either both present, or both omitted
-		# TODO: Do a check real good
+
+		# Signer and signature
+		# TODO: Definitely needs testing
+		security = xsd_optional_security(
+			xml_signer=xml.find("Signer",ns),
+			xml_signature=xml.find("ds:Signature",{"ds":"http://www.w3.org/2000/09/xmldsig#"})
+		)
 
 		# Segments!
-		segment_list = list()
-		for segment in xml.find("SegmentList",ns):
-			segment_list.append(Segment.from_xml(segment,ns))
-
+		segment_list = [Segment.from_xml(segment,ns) for segment in xml.find("SegmentList",ns)]
 
 		return cls(
 			id=id,
@@ -599,12 +671,11 @@ class Cpl:
 			content_originator=content_originator,
 			content_kind=content_kind,
 			tc_start=tc_start,
-			runtime=runtime,
+			total_runtime=runtime,
 			content_versions=content_versions,
 			locales=locale_list,
 			extension_properties=extensions_list,
-			signer=signer,
-			signature=signature
+			security=security
 		)
 	
 	@property
--- tests/test_cpl.py
+++ tests/test_cpl.py
@@ -1,29 +1,54 @@
 from imflib import cpl
-from posttools import timecode
 import sys, pathlib
 
 if len(sys.argv) < 2:
-	sys.exit("Usage: test_cpl.py path_to_imf [path_to_imf ...]")
+	sys.exit("Usage: test_cpl.py path_to_cpl [path_to_cpl ...]")
 
-for path_imf in sys.argv[1:]:
-	for path_cpl in pathlib.Path(path_imf).glob("CPL_*.xml"):
+for path_cpl in sys.argv[1:]:
+	print("\n---\n")
+	print(f"{path_cpl}")
+	
+	try:
 		imf_cpl = cpl.Cpl.from_file(path_cpl)
-		print(f"{path_cpl}")
-		print(f"CPL Name: {imf_cpl.title}")
-		if imf_cpl.annotation: print(imf_cpl.annotation)
-		print(f"Timecode: {imf_cpl.timecode_range}")
+	except Exception as e:
+		print(f"Skipping: {e}")
+		continue
+	
+	print(f"CPL Name: {imf_cpl.title}")
+	print(f"Issued {imf_cpl.issue_date} by {imf_cpl.issuer} using {imf_cpl.creator}")
+	print(f"Has edit rate: {imf_cpl.edit_rate}")
+	if imf_cpl.annotation: print(f"Has annotation: {imf_cpl.annotation}")
+	if imf_cpl.tc_start: print(f"Has start timecode: {imf_cpl.tc_start}")
+	if imf_cpl.total_runtime: print(f"Has total runtime: {imf_cpl.total_runtime}")
+	if imf_cpl.content_originator: print(f"Contains {imf_cpl.content_kind or ''} content from {imf_cpl.content_originator}")
+	if imf_cpl.content_versions:
+		print(f"Has content versions:")
+		for version in imf_cpl.content_versions:
+			print(f"\t{version}")
+	if imf_cpl.locales:
+		print(f"Has locales:")
+		for locale in imf_cpl.locales:
+			print(f"\t{locale}")
+	if imf_cpl.extension_properties:
+		print("Has extensions:")
+		for ext in imf_cpl.extension_properties:
+			print(f"\t{ext}")
+	
+	
 
-		reslist = list(imf_cpl.resources)
-		print(f"Contains {len(list(reslist))} resources in {len(list(imf_cpl.sequences))} sequences across {len(list(imf_cpl.segments))} segment(s):")
-		print(f"  {len([res for res in reslist if isinstance(res,cpl.ImageResource)])} videos, {len([res for res in reslist if isinstance(res, cpl.AudioResource)])} audio")
+	continue
+
+	reslist = list(imf_cpl.resources)
+	print(f"Contains {len(list(reslist))} resources in {len(list(imf_cpl.sequences))} sequences across {len(list(imf_cpl.segments))} segment(s):")
+	print(f"  {len([res for res in reslist if isinstance(res,cpl.ImageResource)])} videos, {len([res for res in reslist if isinstance(res, cpl.AudioResource)])} audio")
+	
+	for idx_seg, seg in enumerate(imf_cpl.segments):
+		print(f"    SEGMENT {idx_seg+1}:")
 		
-		for idx_seg, seg in enumerate(imf_cpl.segments):
-			print(f"    SEGMENT {idx_seg+1}:")
-			
-			for idx_seq, seq in enumerate(seg.sequences):
-				print(f"    | SEQUENCE {idx_seq+1} ({type(seq).__name__}):    {seq.timecode_range}")
+		for idx_seq, seq in enumerate(seg.sequences):
+			print(f"    | SEQUENCE {idx_seq+1} ({type(seq).__name__}):    {seq.timecode_range}")
 
-				for idx_res, res in enumerate(seq.resources):
-					print(f"    |- RESOURCE {str(idx_res+1).rjust(3)} ({type(res).__name__}):    {res.timecode_range}    {res.edit_range}  {'x' + str(res.repeat_count) if res.repeat_count else ''}")
+			for idx_res, res in enumerate(seq.resources):
+				print(f"    |- RESOURCE {str(idx_res+1).rjust(3)} ({type(res).__name__}):    {res.timecode_range}    {res.edit_range}  {'x' + str(res.repeat_count) if res.repeat_count else ''}")
 
-		print("---")
+	print("---")
