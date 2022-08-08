60c9316045e1329bfc7c43bee0df2aa0cce71c4b
mjordan <michael@glowingpixel.com>
2022-08-07 20:11:20 -0700

    More colors

--- imflib/colorschemes.py
+++ imflib/colorschemes.py
@@ -120,7 +120,7 @@ class Rec709YCbCr8(ColorScheme):
 	"""8-bit YCbCr components with COLOR.3 colorimetry and QE.1 quanitization"""
 
 	y:int
-	"""Luminence"""
+	"""Luminance"""
 
 	cb:int
 	"""Chroma B"""
@@ -139,11 +139,11 @@ class Rec709YCbCr8(ColorScheme):
 		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 255, 3, int)
 
 @dataclasses.dataclass(frozen=True)
-class Rec709RGB10(ColorScheme):
+class Rec709YCbCr10(ColorScheme):
 	"""10-bit YCbCr components with COLOR.3 colorimetry and QE.1 quanitization"""
 
 	y:int
-	"""Luminence"""
+	"""Luminance"""
 
 	cb:int
 	"""Chroma B"""
@@ -160,3 +160,542 @@ class Rec709RGB10(ColorScheme):
 	def __post_init__(self):
 		"""Validate"""
 		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 1023, 3, int)
+
+# Extended-Gamut YCbCr
+
+@dataclasses.dataclass(frozen=True)
+class XvYCCYCbCr8(ColorScheme):
+	"""8-bit Extended-Gamut YCbCr components with COLOR.4 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 255, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class XvYCCYCbCr10(ColorScheme):
+	"""10-bit Extended-Gamut YCbCr components with COLOR.4 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 1023, 3, int)
+
+# Rec2020
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020YCbCr10(ColorScheme):
+	"""10-bit Rec2020 YCbCr components with COLOR.5 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020YCbCr12(ColorScheme):
+	"""12-bit Rec2020 YCbCr components with COLOR.5 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020RGB10(ColorScheme):
+	"""10-bit RGB components with COLOR.5 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020RGB12(ColorScheme):
+	"""10-bit RGB components with COLOR.5 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020FullRGB10(ColorScheme):
+	"""10-bit full-range RGB components with COLOR.5 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Rec2020FullRGB12(ColorScheme):
+	"""12-bit full-range RGB components with COLOR.5 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+# COLOR6
+
+@dataclasses.dataclass(frozen=True)
+class Color6RGB10(ColorScheme):
+	"""10-bit RGB components with COLOR.6 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color6FullRGB10(ColorScheme):
+	"""10-bit full-range RGB components with COLOR.6 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color6RGB12(ColorScheme):
+	"""12-bit RGB components with COLOR.6 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color6FullRGB12(ColorScheme):
+	"""12-bit full-range RGB components with COLOR.6 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color6RGB16(ColorScheme):
+	"""16-bit RGB components with COLOR.6 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 65535, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color6FullRGB16(ColorScheme):
+	"""16-bit full-range RGB components with COLOR.6 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 65535, 3, int)
+
+# COLOR7
+
+@dataclasses.dataclass(frozen=True)
+class Color7YCbCr10(ColorScheme):
+	"""10-bit YCbCr components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7YCbCr12(ColorScheme):
+	"""12-bit YCbCr components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7YCbCr16(ColorScheme):
+	"""16-bit YCbCr components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	y:int
+	"""Luminance"""
+
+	cb:int
+	"""Chroma B"""
+	
+	cr:int
+	"""Chroma R"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		y,cb,cr = (int(val) for val in xml.text.split())
+		return cls(y=y, cb=cb, cr=cr)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.y,self.cb,self.cr), 0, 65535, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7RGB10(ColorScheme):
+	"""10-bit RGB components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7FullRGB10(ColorScheme):
+	"""10-bit full-range RGB components with COLOR.7 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 1023, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7RGB12(ColorScheme):
+	"""12-bit RGB components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7FullRGB12(ColorScheme):
+	"""12-bit full-range RGB components with COLOR.7 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 4095, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7RGB16(ColorScheme):
+	"""16-bit RGB components with COLOR.7 colorimetry and QE.1 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 65535, 3, int)
+
+@dataclasses.dataclass(frozen=True)
+class Color7FullRGB16(ColorScheme):
+	"""16-bit full-range RGB components with COLOR.7 colorimetry and QE.2 quanitization"""
+
+	r:int
+	"""Red primary"""
+
+	g:int
+	"""Green primary"""
+	
+	b:int
+	"""Blue primary"""
+
+	@classmethod
+	def from_xml(cls, xml:et.Element, ns:typing.Optional[dict]=None):
+		"""Parse from XML"""
+		r,g,b = (int(val) for val in xml.text.split())
+		return cls(r=r, g=g, b=b)
+	
+	def __post_init__(self):
+		"""Validate"""
+		self.xsd_validate_color_values((self.r,self.g,self.b), 0, 65535, 3, int)
